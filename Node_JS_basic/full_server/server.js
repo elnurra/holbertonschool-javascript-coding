634159f027403f6688a7b666b326d753375891a4
elnurra <elnurra@code.edu.az>
2024-05-26 11:55:33 +0400

    feat: added full server task

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import mapRoutes from './routes';
+
+const app = express();
+const PORT = 1245;
+
+mapRoutes(app);
+app.listen(PORT, () => {
+  console.log(`Server listeni
